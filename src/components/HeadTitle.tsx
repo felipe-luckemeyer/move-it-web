c177e10525c7c40b1231890f2c05abcb57bc9465
Felipe Lückemeyer <felipe.luckemeyer10@gmail.com>
2021-02-27 20:08:59 -0300

    melhoria terminada

--- src/components/HeadTitle.tsx
+++ src/components/HeadTitle.tsx
@@ -7,12 +7,15 @@ export default function HeadTitle() {
   const { isActive, minutes, seconds, hasFinished } = useContext(CountdownContext);
   const [minuteLeft, minuteRight] = formatTime(minutes);
   const [secondLeft, secondRight] = formatTime(seconds);
-  const time = `${minuteLeft}${minuteRight}:${secondLeft}${secondRight}`;
 
   if (isActive)
     return (
       <Head>
-        <title>⏱ Bora produzir | {time}</title>
+        <title>
+          ⏱ Bora produzir | {minuteLeft}
+          {minuteRight}:{secondLeft}
+          {secondRight}
+        </title>
       </Head>
     );
 
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,7 +1,3 @@
-// interface FormatTimeProps {
-//   time: string;
-// }
-
 export const formatTime = (time) => {
   return String(time).padStart(2, '0').split('');
 };
